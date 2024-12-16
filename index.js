083c342952fd652ad34c044662e197dcc0651dc4
Quang Huy <huytranquang.150701@gmail.com>
2024-12-16 17:46:34 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: process.env.FRONT_END_URL } });
+const io = new Server(server, { cors: { origin: "*" } });
 const PORT = process.env.PORT || 5000;
 
 let users = [];
@@ -19,7 +19,9 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: true,
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
